56313e5009488677254b07529abd102b1cd23584
Mike Dumlao <mikhaild908@gmail.com>
2019-07-09 16:35:46 -0700

    Create HitsDetails.cs

---
+++ HitsDetails.cs
@@ -0,0 +1,12 @@
+using System;
+namespace OrderLineGeo
+{
+    public class HitsDetails
+    {
+        public string _index { get; set; }
+        public string _type { get; set; }
+        public string _id { get; set; }
+        public double _score { get; set; }
+        public Source _source { get; set; }
+    }
+}
