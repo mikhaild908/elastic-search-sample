67a6d5fe4db73608142c58a908ee549f81024d11
Mike Dumlao <mikhaild908@gmail.com>
2019-07-09 16:36:48 -0700

    Create Shards.cs

---
+++ Shards.cs
@@ -0,0 +1,11 @@
+using System;
+namespace OrderLineGeo
+{
+    public class Shards
+    {
+        public int total { get; set; }
+        public int successful { get; set; }
+        public int skipped { get; set; }
+        public int failed { get; set; }
+    }
+}
